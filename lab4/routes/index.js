1825b4ed523ea528930b74d63c1d44a36bd53eba
David-McNiven <davidmcniven@bell.net>
2016-09-30 17:20:03 -0400

    Added nephew and navbar

--- lab4/routes/index.js
+++ lab4/routes/index.js
@@ -4,10 +4,26 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'My Family' });
+});
+
+router.get('/david', function(req, res, next){
   res.render('david', { title: 'David'});
+});
+
+router.get('/steph', function(req,res, next){
   res.render('steph', { title: 'Steph'});
+});
+
+router.get('/shane', function(req,res, next){
+  res.render('shane', { title: 'Shane'});
+});
+
+router.get('/don', function(req,res, next){
   res.render('don', { title: 'Don'});
-  res.render('diana', { title: 'Diana'})
+});
+
+router.get('/diana', function(req,res, next){
+  res.render('diana', { title: 'Diana'});
 });
 
 module.exports = router;
